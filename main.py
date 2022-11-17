eafcee65161dc6f3cf7438d980a6df9a6fd4b0b1
jubesss <105614957+jubesss@users.noreply.github.com>
2022-11-16 22:16:53 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+n1 = float(input('digite a nota do primeiro bi '))
+n2 = float(input('digite a nota do segundo bi' ))
+n3 = float(input('digite a nota do terceiro bi '))
+n4 = float(input('digite a nota do quarto bi '))
+ma = (n1 + n2 + n3 + n4)
+print('sua média anual foi de {}',(ma))
+if ma >= 24.0:
+    print('PASSOU PORRA PABABUAINS')
+else:
+    print('VOCE FOI DE ARRASTA PRA CIMA (F)')
